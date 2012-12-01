213b2b54b4b1f5828c0fb8a0a2f4aa4e25cfd3cf
Michael Limiero <mike5713@gmail.com>
2012-12-01 04:26:15 -0500

    Make a new freaking file!

--- EditorPage.xaml.cs
+++ EditorPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Storage;
 using Windows.Storage.Pickers;
+using Windows.Storage.Provider;
 using Windows.UI.ViewManagement;
 using MarkdownDeep;
 
@@ -157,9 +158,41 @@ namespace NerdNote
             ShowWebView();
         }
 
-        private void NewFile(object sender, RoutedEventArgs e)
+        private async void NewFile(object sender, RoutedEventArgs e)
         {
+            bool unsnapped = ((ApplicationView.Value != ApplicationViewState.Snapped) || ApplicationView.TryUnsnap());
+            if (!unsnapped)
+            {
+                return;
+            }
+            FileSavePicker savePicker = new FileSavePicker();
+            savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            // Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add("Markdown", new List<string>() { ".md" });
+            // Default file name if the user does not type one in or select a file to replace
+            savePicker.SuggestedFileName = "New Note";
 
+            StorageFile file = await savePicker.PickSaveFileAsync();
+            if (file != null)
+            {
+                // Prevent updates to the remote version of the file until we finish making changes and call CompleteUpdatesAsync.
+                CachedFileManager.DeferUpdates(file);
+                // write to file
+                await FileIO.WriteTextAsync(file, "");
+                // Let Windows know that we're finished changing the file so the other app can update the remote version of the file.
+                // Completing updates may require Windows to ask for user input.
+                FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
+                if (status == FileUpdateStatus.Complete)
+                {
+                    LoadFile(file);
+                }
+                else
+                {
+                }
+            }
+            else
+            {
+            }
         }
 
         private async void OpenFile(object sender, RoutedEventArgs e)
